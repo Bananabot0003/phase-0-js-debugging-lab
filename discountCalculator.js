086b99c941fb5567d90dafc9020d927be46f96c7
bananabot0003 <hunterw1201@gmail.com>
2026-09-11 18:21:06 -0500

    completed lab Hunter Williams

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,9 +1,11 @@
+// using debugger to find the bug in the code
+debugger
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
     debugger
-    for (let i = 1; i < quantity; i++) {
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
-    }
+    }// found the bug in the let i = 1 which needed to start at 0
 
     if (quantity >= 10) {
         totalPrice *= 0.9;
